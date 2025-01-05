98d7e9925fe977ccfab6aecabb846e0913f435c2
Manuel Duran <durbonca@gmail.com>
2025-01-05 12:03:08 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import alpinejs from '@astrojs/alpinejs';
+
+import db from '@astrojs/db';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), alpinejs(), db()],
+  adapter: netlify()
+});
